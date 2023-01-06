81b5fd0396152890a48eace9be81f36fea005564
jacksonkstenger <jacksonkstenger@gmail.com>
2023-01-06 14:47:19 -0800

    improved winning functionality

--- chinese-checkers/src/components/timer.jsx
+++ chinese-checkers/src/components/timer.jsx
@@ -4,7 +4,7 @@ export default function Timer({myProps, updateMyProps}) {
     return (
         <div className="timer" >
             <p>I am TIMER</p>
-            <p>{myProps.winner}</p>
+            <p>{(myProps.winner === 1 && "You win!") || (myProps.winner === 2 && "You lost, better luck next time") || (myProps.winner === 0 && "Game on!")}</p>
         </div>
     )
 }
